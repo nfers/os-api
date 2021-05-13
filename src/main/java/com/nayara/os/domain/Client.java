944a4cdb1332d1d48f52d31ed2fc33522d782a36
Nayara Christina Ferreira dos Santos <nayara.christina@totvs.com.br>
2021-05-12 22:08:06 -0300

    adicionando as classes de domínio

---
+++ src/main/java/com/nayara/os/domain/Client.java
@@ -0,0 +1,13 @@
+package com.nayara.os.domain;
+
+public class Client extends Person {
+
+	public Client() {
+		super();
+	}
+
+	public Client(Integer id, String name, String cpf, String phone) {
+		super(id, name, cpf, phone);
+	}
+	
+}
---
+++ src/main/java/com/nayara/os/domain/Technique.java
@@ -0,0 +1,15 @@
+package com.nayara.os.domain;
+
+public class Technique extends Person {
+
+	public Technique() {
+		super();
+
+	}
+
+	public Technique(Integer id, String name, String cpf, String phone) {
+		super(id, name, cpf, phone);
+
+	}
+
+}
